406d2af61e53006237e3248d3ac1d4d0ab10b9d7
Damla Koksal <me@damlakoksal.com>
2021-12-17 16:18:53 +0300

    style: extend eslint plugin to prevent "missing React" error

---
+++ src/pages/Error/index.tsx
@@ -0,0 +1,5 @@
+// import styles from "./styles.module.scss";
+
+export default function Error() {
+  return <div>error404</div>;
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+// import styles from "./styles.module.scss"
+
+export default function Home() {
+  return <div>Layout</div>;
+}
