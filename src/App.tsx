23d481607a27032470a6f335ca07e2f1d0b47b7b
Damla Koksal <me@damlakoksal.com>
2021-12-17 17:35:12 +0300

    feat: implement react-router-dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { Button } from './components';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, Error } from './pages/';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button>Click</Button>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <p>Learn React</p>
+              <Home />
+            </div>
+          }
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,11 @@
 // import styles from "./styles.module.scss"
+import { Button } from '../../components';
 
 export default function Home() {
-  return <div>Layout</div>;
+  return (
+    <div>
+      Layout
+      <Button>Test</Button>
+    </div>
+  );
 }
